a2fa46fe1020e7a9b54cae475802675ec09cf2b1
Francesco Torregrossa <d.ftorregrossa@gmail.com>
2020-03-14 18:35:15 +0100

    bug fix tcp

--- sniffer.py
+++ sniffer.py
@@ -127,15 +127,15 @@ def icmp_packet(data):
 
 
 def tcp_seg(data):
-    (src_port, destination_port, sequence, acknowledgenment,
+    (src_port, dest_port, sequence, acknowledgement,
      offset_reserv_flag) = struct.unpack('! H H L L H', data[:14])
     offset = (offset_reserv_flag >> 12) * 4
-    flag_urg = (offset_reserved_flag & 32) >> 5
-    flag_ack = (offset_reserved_flag & 32) >> 4
-    flag_psh = (offset_reserved_flag & 32) >> 3
-    flag_rst = (offset_reserved_flag & 32) >> 2
-    flag_syn = (offset_reserved_flag & 32) >> 1
-    flag_fin = (offset_reserved_flag & 32) >> 1
+    flag_urg = (offset_reserv_flag & 32) >> 5
+    flag_ack = (offset_reserv_flag & 32) >> 4
+    flag_psh = (offset_reserv_flag & 32) >> 3
+    flag_rst = (offset_reserv_flag & 32) >> 2
+    flag_syn = (offset_reserv_flag & 32) >> 1
+    flag_fin = (offset_reserv_flag & 32) >> 1
 
     return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
 
