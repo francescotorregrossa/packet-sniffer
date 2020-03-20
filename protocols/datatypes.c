b46594fa86f40b28e9bf117706654b145c793006
francescotorregrossa <d.ftorregrossa@gmail.com>
2020-03-20 23:45:49 +0100

    fix switch_encoding_dw

--- protocols/datatypes.c
+++ protocols/datatypes.c
@@ -9,7 +9,9 @@ word switch_encoding_w(word w)
 
 dword switch_encoding_dw(dword dw)
 {
-  dword lower = (dw & (0b11111111000000001111111100000000)) >> 8;
-  dword upper = (dw & (0b00000000111111110000000011111111)) << 8;
-  return lower + upper;
+  dword a = (dw & (0b11111111000000000000000000000000)) >> 24;
+  dword b = (dw & (0b00000000111111110000000000000000)) >> 8;
+  dword c = (dw & (0b00000000000000001111111100000000)) << 8;
+  dword d = (dw & (0b00000000000000000000000011111111)) << 24;
+  return a + b + c + d;
 }
